4f4cd2f695d3e46fb5d7c5b3a61070a68d4edc0f
Renee Vandervelde <Renee@ReneeVandervelde.com>
2020-07-11 07:46:54 -0500

    Add SSID types

--- structures/src/commonMain/kotlin/com/inkapplications/karps/structures/Address.kt
+++ structures/src/commonMain/kotlin/com/inkapplications/karps/structures/Address.kt
@@ -1,5 +1,7 @@
 package com.inkapplications.karps.structures
 
+import com.inkapplications.karps.structures.SsidType.*
+
 /**
  * Callsign/SSID pair used in AX.25 to identify a station.
  */
@@ -10,5 +12,23 @@ data class Address(
     init {
         require(ssid in 0..15) { "Address SSID must be 0-15" }
     }
-}
 
+    /**
+     * Assumed SSID type based on recommended categories.
+     */
+    val type: SsidType = when (ssid.toInt()) {
+        0 -> Primary
+        in 1..4, 15 -> Generic
+        5 -> Network
+        6 -> Special
+        7 -> Ht
+        8 -> Boat
+        9 -> Mobile
+        10 -> Internet
+        11 -> Air
+        12 -> Tracker
+        13 -> Weather
+        14 -> Trucking
+        else -> throw IllegalArgumentException("SSID must be 0-15")
+    }
+}
