d0701de90738674a7437ca88c66b9601cf53f8d4
Renee Vandervelde <Renee@ReneeVandervelde.com>
2022-02-12 20:45:09 -0600

    Add publishing scripts

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,5 +7,6 @@ repositories {
 }
 dependencies {
     implementation(libraries.kotlin.gradle)
-    implementation("com.jakewharton.mosaic:mosaic-gradle-plugin:0.1.0")
+    implementation(libraries.mosaic)
+    implementation(libraries.ink.publishing)
 }
--- buildSrc/src/main/kotlin/library.multiplatform.gradle.kts
+++ buildSrc/src/main/kotlin/library.multiplatform.gradle.kts
@@ -1,7 +1,38 @@
 plugins {
     kotlin("multiplatform")
+    id("maven-publish")
 }
 
 kotlin {
     jvm()
+}
+
+publishing {
+    publications {
+        withType<MavenPublication> {
+            pom {
+                name.set("ACK ${project.name}")
+                description.set("APRS Client for Kotlin")
+                url.set("https://ack.inkapplications.com")
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://choosealicense.com/licenses/mit/")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("reneevandervelde")
+                        name.set("Renee Vandervelde")
+                        email.set("Renee@InkApplications.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/InkApplications/ack.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:InkApplications/ack.git")
+                    url.set("https://github.com/InkApplications/ack")
+                }
+            }
+        }
+    }
 }
